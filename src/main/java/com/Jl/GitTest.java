ea75368b1382f35e055b6a0f8394bb0071aa3507
Jl <Jl@163.com>
2022-06-21 10:50:02 +0800

    first commit

---
+++ src/main/java/com/Jl/GitTest.java
@@ -0,0 +1,11 @@
+package com.Jl;
+
+/**
+ * @author name
+ * @create 2022-06-21 10:35
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
